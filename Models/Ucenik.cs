ef72de4620cd784ef81fff1cb8a3592947113df8
peet53 <petarivkovic53@gmail.com>
2017-03-26 21:05:26 +0200

    Add files via upload

---
+++ Models/Ucenik.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projekat_ITS.Models
+{
+    public class Ucenik
+    {
+        
+        [Key]
+        public int id { get; set; }
+    
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        [DisplayName("Ime Roditelja")]
+        public string ImeRoditelja { get; set; }
+        public int JMBG { get; set; }
+        [DisplayName("Datum rodjenja")]
+        public string DatumRodjenja { get; set; }
+        [DisplayName("Mesto rodjenja")]
+        public string MestoRodjenja { get; set; }
+        [DisplayName("Opstina rodjenja")]
+        public string OpstinaRodjenja { get; set; }
+        public string Pol { get; set; }
+        [DisplayName("Broj licne karte")]
+        public int BrojLicneKarte { get; set; }
+        [DisplayName("Broj telefona")]
+        public string BrojTelefona { get; set; }
+        public string Email { get; set; }
+        public string Adresa{ get; set; }
+        public string Beleska { get; set; }
+        
+    
+    }
+    
+    
+
+
+    
+    
+    
+    
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projekat_ITS.Models
+{
+    public class User
+    {
+        public string LogIn { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
